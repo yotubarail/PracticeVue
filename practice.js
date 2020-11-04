af6d590f6ed7628902be8618fd59996825b58efa
yotubarail <shota_t@i.softbank.jp>
2020-11-04 17:04:25 +0900

    app2のtitileを変更

--- practice.js
+++ practice.js
@@ -4,3 +4,10 @@ var app = new Vue ({
         message: "Hello World"
     }
 })
+
+var app2 = new Vue ({
+    el: ".app2",
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})

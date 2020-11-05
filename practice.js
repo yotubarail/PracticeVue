9839121cfdc0da2e79f3a2555b02fded5d4fdf38
yotubarail <shota_t@i.softbank.jp>
2020-11-05 14:39:34 +0900

    条件分岐

--- practice.js
+++ practice.js
@@ -11,3 +11,10 @@ var app2 = new Vue ({
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 })
+
+var app3 = new Vue ({
+    el: "#app3",
+    data: {
+        seen: true
+    }
+})

2dac7f9b18d75044d66640476e739291dc8ae458
yotubarail <shota_t@i.softbank.jp>
2020-11-18 11:19:53 +0900

    Vue-cliを用いてVueを扱う

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <FileUpload></FileUpload>
+</template>
+
+<script>
+import FileUpload from "@/components/FileUpload"
+
+  export default {
+    components: {
+      FileUpload
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label>
+        ファイルアップロード
+        <input type="file">
+    </label>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    label input {
+        display: none;
+    }
+
+    label {
+        padding: 0 1rem;
+        border: solid 1px #888;
+    } 
+</style>

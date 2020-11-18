4b57e514e53cd4eca063538e6a1190c0c3293a26
yotubarail <shota_t@i.softbank.jp>
2020-11-18 16:32:55 +0900

    ファイルアップロードとプレビュー表示

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -2,13 +2,35 @@
   <div>
     <label>
         ファイルアップロード
-        <input type="file">
+        <input type="file" ref="file" @change="setImage">
     </label>
+    <img :src="data.image" alt="">
+    <p>{{ data.name }}</p>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {
+        data: {
+          name: "",
+          image: ""
+        }
+      };
+    },
+    methods: {
+      setImage(e) {
+        const files = this.$refs.file;
+        const fileImg = files.files[0];
+        if (fileImg.type.startsWith("image/")) {
+          this.data.image = window.URL.createObjectURL(fileImg);
+          this.data.name = fileImg.name;
+          this.data.type = fileImg.type;
+        }
+      },
+    }
+  }
 </script>
 
 <style>
@@ -20,4 +42,8 @@
         padding: 0 1rem;
         border: solid 1px #888;
     } 
+    
+    img {
+      width: 20%;
+    }
 </style>

eb36144a5cb0625d91db98b391efa628014ecf93
yotubarail <shota_t@i.softbank.jp>
2020-11-26 15:34:05 +0900

    v-bindで送信ボタンを押すたびにh1のstyleが変わる

--- src/components/DataBind.vue
+++ src/components/DataBind.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>$emitでデータ受け渡し</h1>
+        <h1 :class={style:isActive}>$emitでデータ受け渡し</h1>
         <input type="text" v-model="text" placeholder="入力">
         <button @click="click">送信</button>
     </div>
@@ -10,17 +10,22 @@
 export default {
     data() {
         return {
-            text: ""
+            text: "",
+            isActive: true
         }
     },
     methods: {
         click() {
-            this.$emit("emit", this.text)
+            this.$emit("emit", this.text);
+            this.isActive = !this.isActive
         }
     }
 }
 </script>
 
 <style>
-
+.style {
+    color: red;
+    font-size: 3rem;
+}
 </style>
